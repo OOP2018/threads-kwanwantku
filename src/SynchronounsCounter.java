871e5484ef9916618e6c75d5093aa59d4b0a8f29
kwanwantku <kwanwan.t@ku.th>
2018-03-30 15:37:06 +0700

    add 3.

---
+++ src/SynchronounsCounter.java
@@ -0,0 +1,9 @@
+public class SynchronounsCounter extends Counter {
+	@Override
+	public synchronized void add(int amount) {
+		// To avoid the overhead of calling the superclass add,
+		// define the superclass attribute (total) as protected and
+		// directly add to the total here. Don't define a new attribute
+		// in this class!
+	}
+}
--- src/ThreadSum.java
+++ src/ThreadSum.java
@@ -3,7 +3,7 @@ public class ThreadSum {
 		// upper limit of numbers to add/subtract to Counter
 		final int LIMIT = 10000000;
 		// The counter that accumulates a total.
-		Counter counter = new Counter();
+		Counter counter = new CounterWithLock();
 		runThreads(counter, LIMIT);
 	}
 
